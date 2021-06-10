1ca445ee0bbbce2105792d9dcb6abede890f7937
abdoum <77646461+abdoum@users.noreply.github.com>
2021-06-10 16:27:37 +0200

    Initial Commit

---
+++ inspire/inspireApp.swift
@@ -0,0 +1,20 @@
+//
+//  inspireApp.swift
+//  inspire
+//
+//  Created by ab on 10/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct inspireApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
