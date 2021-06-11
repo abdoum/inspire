f04efed1d014aa5341ae1745fdfc0fadf76b1374
abdoum <77646461+abdoum@users.noreply.github.com>
2021-06-11 16:01:49 +0200

    restructured project directories

---
+++ inspire/views/ExperiencesHome/Components/ExperienceCard.swift
@@ -0,0 +1,20 @@
+//
+//  ExperienceCard.swift
+//  inspire
+//
+//  Created by ab on 11/06/2021.
+//
+
+import SwiftUI
+
+struct ExperienceCard: View {
+    var body: some View {
+        Text("Image this as an experience card!")
+    }
+}
+
+struct ExperienceCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ExperienceCard()
+    }
+}
---
+++ inspire/views/ExperiencesHome/View/ExperiencesHome.swift
@@ -0,0 +1,20 @@
+//
+//  ExperiencesHome.swift
+//  inspire
+//
+//  Created by ab on 11/06/2021.
+//
+
+import SwiftUI
+
+struct ExperiencesHome: View {
+    var body: some View {
+        ExperienceCard()
+    }
+}
+
+struct ExperiencesHome_Previews: PreviewProvider {
+    static var previews: some View {
+        ExperiencesHome()
+    }
+}
