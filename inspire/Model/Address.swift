7bef943583df769f5d5964d621a6663a0e60a62f
abdoum <77646461+abdoum@users.noreply.github.com>
2021-06-16 22:40:42 +0200

    added mock data and fixed data model issue causing circular type

--- inspire/Model/Address.swift
+++ inspire/Model/Address.swift
@@ -15,3 +15,11 @@ struct Address {
     var country : String?
 }
 
+var MOCK_ADDRESSES = [
+    Address(street: "0350 Continental Pass", city: "Saint-Amand-les-Eaux", zipCode: "59734 CEDEX", state: "Nord-Pas-de-Calais", country: "FR"),
+    Address(street: "78779 Mallard Pass", city: "Reims", zipCode: "51715 CEDEX", state: "Champagne-Ardenne", country: "FR"),
+    Address(street: "9716 Calypso Circle", city: "Nice", zipCode: "06182 CEDEX 2", state: "Provence-Alpes-Côte d'Azur", country: "FR"),
+    Address(street: "473 Larry Junction", city: "Romilly-sur-Seine", zipCode: "10104 CEDEX", state: "Champagne-Ardenne", country: "FR"),
+    Address(street: "1150 Vernon Avenue", city: "Xiongguan", zipCode: "", state: "", country: "CN"),
+    Address(street: "306 Reinke Court", city: "Montpellier", zipCode: "34032 CEDEX 1", state: "Languedoc-Roussillon", country: "FR")
+]
